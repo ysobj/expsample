a065f19aae456dee92bd389a46a1a9f735473039
ysobj <litailang+ysobj@gmail.com>
2016-03-01 13:04:55 +0900

    using hbs template

--- routes/rooms.js
+++ routes/rooms.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET rooms listing. */
 router.get('/:room/', function(req, res, next) {
-  res.send('this room is ' + req.params.room);
+  //res.send('This room is ' + req.params.room);
+  res.render('rooms', {
+    roomName: req.params.room
+  });
 });
 
 module.exports = router;
