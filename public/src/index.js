d6f4ab0f271b13ffee898167702a00b879609d51
ysobj <litailang+ysobj@gmail.com>
2016-03-22 08:27:14 +0900

    using react with jsx

--- public/src/index.js
+++ public/src/index.js
@@ -2,3 +2,24 @@ $ = jQuery = require('jquery');
 require('bootstrap');
 var React = require('react');
 var ReactDom = require('react-dom');
+
+var Message = React.createClass({
+  render: function() {
+        var renderMessage = function() {
+                  return (
+      <div>
+        <div className="message-container">
+          <div className="avatar"><img alt="avatar" src='http://www.gravatar.com/avatar/13f5b1351a04db4c83e60fcf7fb6d6ae?rating=PG&size=50&default=wavatar'/></div>
+          <div className="comment-info"><div>ysobj</div>3/11 12:40</div></div>
+        <div>いろはにほへと ちりぬぬを</div>
+      </div>
+                    );
+                }
+    return <div>
+      {renderMessage()}
+      {renderMessage()}
+    </div>;
+  }
+});
+var root = document.getElementById('root');
+ReactDom.render(React.createElement(Message), root);
