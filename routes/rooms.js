692ce8efc6f563641174efff065502b7b519f593
ysobj <litailang+ysobj@gmail.com>
2016-04-14 12:39:42 +0900

    20160414

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
+var comments;
+
+mongodb.MongoClient.connect('mongodb://localhost:27017/expsample',function(err,db){
+  comments = db.collection('comments');
+});
 
 /* GET rooms listing. */
 router.get('/:room/', function(req, res, next) {
+  console.log('rooms');
   //res.send('This room is ' + req.params.room);
-  res.render('rooms', {
-    roomName: req.params.room
+  comments.find().toArray(function(err,comments){
+    console.log(comments);
+    res.render('rooms', {
+      roomName: req.params.room
+    });
   });
 });
 
